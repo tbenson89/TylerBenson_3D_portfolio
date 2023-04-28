910168d7a36143dad849490780d62f483b8e68cd
tbenson89 <typrobenson@gmail.com>
2023-04-28 15:43:50 -0600

    FIX deploy issue for laptop obj

--- src/components/canvas/Laptop.jsx
+++ src/components/canvas/Laptop.jsx
@@ -4,7 +4,7 @@ import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
 import CanvasLoader from '../Loader'
 
 const Laptop = () => {
-  const laptop = useGLTF('../cyberpunk_laptop/scene.gltf')
+  const laptop = useGLTF('./cyberpunk_laptop/scene.gltf')
 
   return (
     <mesh>
